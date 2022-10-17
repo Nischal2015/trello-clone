56aa08850588eab8fcfe701ff905205b9ce22036
Nischal Shakya <nischalshakya2015@gmail.com>
2022-10-17 21:02:41 +0545

    feat: implement filtering tasks
    - Use context functions and data to apply filtering of tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css';
 import { AddNewItem, Column } from './components';
+import { useAppState } from './hooks';
 import { AppContainer } from './Styles';
 
 function App() {
+  const { lists } = useAppState();
+
   return (
     <AppContainer>
-      <Column text="Todo:" />
+      {lists.map((list) => (
+        <Column text={list.text} key={list.id} id={list.id} />
+      ))}
       <AddNewItem toggleButtonText="+Add another list" onAdd={console.log} />
     </AppContainer>
   );
--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -1,15 +1,20 @@
+import { useAppState } from '../../hooks';
 import { ColumnContainer, ColumnTitle } from '../../Styles';
 import { AddNewItem } from '../AddNewItem';
 import { Card } from '../Card';
 import { ColumnProps } from './Column.d';
 
-function Column({ text }: ColumnProps) {
+function Column({ text, id }: ColumnProps) {
+  const { getTasksByListId } = useAppState();
+
+  const tasks = getTasksByListId(id);
+
   return (
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
-      <Card text="Generate app scaffold" />
-      <Card text="Learn TypeScript" />
-      <Card text="Begin to use static typing" />
+      {tasks.map((task) => (
+        <Card text={task.text} key={task.id} id={task.id} />
+      ))}
       <AddNewItem toggleButtonText="+ Add another card" onAdd={console.log} dark />
     </ColumnContainer>
   );
