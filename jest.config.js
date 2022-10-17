55689630dbebfca590cbefe0e6862c9a63c74c58
Nischal Shakya <nischalshakya2015@gmail.com>
2022-10-17 19:21:44 +0545

    chore(config): modify config files
    - Change eslint and tsconfig settings

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  testEnvironment: "jsdom",
+  testEnvironment: 'jsdom',
   transform: {
-    "^.+\\.tsx?$": "ts-jest"
+    '^.+\\.tsx?$': 'ts-jest',
   },
   moduleNameMapper: {
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-    "@/(.*)$": "<rootDir>/src/$1",
-    "@/Assets/(.*)$": "<rootDir>/src/assets/$1",
-    "@/Components/(.*)$": "<rootDir>/src/components/$1",
+    '@/(.*)$': '<rootDir>/src/$1',
+    '@/Assets/(.*)$': '<rootDir>/src/assets/$1',
+    '@/Components/(.*)$': '<rootDir>/src/components/$1',
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-}
+};
