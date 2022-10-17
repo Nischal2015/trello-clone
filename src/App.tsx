dd641c09d29c1b24e3bb70602fd788d747251cea
Nischal Shakya <nischalshakya2015@gmail.com>
2022-10-17 13:25:04 +0545

    refactor: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="card">
+        <h1>This is Trello Clone</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
